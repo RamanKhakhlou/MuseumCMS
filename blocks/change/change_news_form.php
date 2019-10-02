56b554e06ba63340a2e117c1d46fff4f649998c6
r.khakhlou <rotorbrest@gmail.com>
2019-10-02 20:02:53 +0300

    Change pages moved to BEM. Updated text encoding to utf-8

--- blocks/change/change_news_form.php
+++ blocks/change/change_news_form.php
@@ -1,20 +1,18 @@
 <?
 	if(isset($_SESSION['name']))
 	{
-		//Выполняется после отправки формы
-		if($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['Ok'] == "Изменить")
+		if($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['Ok'] == "РР·РјРµРЅРёС‚СЊ")
 		{
-			//Обработка введенных данных
 			$id = CleanData($_POST['id'], 'i');
 			$news = CleanData($_POST['news']);
-			//Внесение изменений в БД
+
 			if(!AddErrorsForNews($news))
 			{
 				$result = ChangeNews($id, $news);
 				
 				if(isset($result) && $result != false)
 				{
-					AddSuccessMessage("Новость успешно отредактирована.");
+					AddSuccessMessage("РќРѕРІРѕСЃС‚СЊ СѓСЃРїРµС€РЅРѕ РѕС‚СЂРµРґР°РєС‚РёСЂРѕРІР°РЅР°.");
 				}
 			}
 ?>
@@ -23,11 +21,11 @@
 		}
 	}
 ?>
-<div id='titlesus'>
+<div class='info'>
 	<table>
 		<tr>
 			<td><img src='image/system/add_data.png' width='37px' height='40px' class='add_data'></td>
-			<td><span class='titlesus_h'>Менеджер новостей: редактировать новость</span></td>
+			<td><span class='info__title'>РњРµРЅРµРґР¶РµСЂ РЅРѕРІРѕСЃС‚РµР№: СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊ РЅРѕРІРѕСЃС‚СЊ</span></td>
 		</tr>
 	</table>
 </div>
@@ -35,7 +33,6 @@
 <?
 	if(isset($_SESSION['name']))
 	{
-		//Вывод формы заполнения
 		if($_SERVER['REQUEST_METHOD'] == "POST")
 		{
 			$id = CleanData($_POST["selectedNews"], 'i');
@@ -44,19 +41,19 @@
 			$rowNews = mysql_fetch_assoc($resaultNews);
 ?>
 			<form action = "index.php?actionChange=change_news_form" method = "POST" name = "changeNews">
-				<div id='cont'>
-					<fieldset class='fs'>
-						<legend><span class='legend'>Редактирование новости</span></legend>
+				<div class='form'>
+					<fieldset class='form__fieldset'>
+						<legend><span class='form__legend'>Р РµРґР°РєС‚РёСЂРѕРІР°РЅРёРµ РЅРѕРІРѕСЃС‚Рё</span></legend>
 						<table>
-							<tr class='asdasd'>
-								<td class='number1'>Текст новости<span class='star'>*</span></td>
-								<td><textarea name = "news" class='tarea' required><?= $rowNews["news"]?></textarea></td>
+							<tr class='form__row'>
+								<td class='form__label'>РўРµРєСЃС‚ РЅРѕРІРѕСЃС‚Рё<span class='form__star'>*</span></td>
+								<td><textarea name = "news" class='form__area' required><?= $rowNews["news"]?></textarea></td>
 							</tr>
 						</table>
 					</fieldset>
-					<input type = "hidden" name = "id" value = "<?= $rowNews['id']?>" class="buttonw">
-					<input type = "submit" name = "Ok" value = "Изменить" class="buttonw">
-					<input type = "button" name = "back" value = "Назад" class="buttonw" onclick = "museum.redirect('Change', 'change_news_all')">
+					<input type = "hidden" name = "id" value = "<?= $rowNews['id']?>" class='form__button'>
+					<input type = "submit" name = "Ok" value = "РР·РјРµРЅРёС‚СЊ" class='form__button'>
+					<input type = "button" name = "back" value = "РќР°Р·Р°Рґ" class='form__button' onclick = "museum.redirect('Change', 'change_news_all')">
 				</div>
 			</form>
 <?
